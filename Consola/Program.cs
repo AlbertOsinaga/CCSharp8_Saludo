d48a9db82683d89deeb390ca5f5b6a50039c515e
Alberto Osinaga <luis.osinaga@gmail.com>
2020-04-04 15:04:02 -0400

    Initial

---
+++ Consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace Consola
+{
+	class Program
+	{
+		static void Mainn()
+		{
+			// Despliegue en la Pantalla: “Por favor introduzca su nombre: ”
+			WriteLine("Por favor introduzca su nombre: ");
+
+			// Cuando el Usuario termine de escribir su nombre. Leálo
+			var usuario = ReadLine();
+
+			// Despliegue en la pantalla el saludo: “Hola[Nombre del Usuario]!”
+			WriteLine($"Hola {usuario}!");
+		}
+	}
+}
+
---
+++ Consola/Programa2.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace Consola
+{
+	class Programa2
+	{
+		static void Main()
+		{
+			// Despliegue en la Pantalla: “Por favor introduzca su nombre: ”
+			Write("Por favor introduzca su nombre: ");
+
+			// Cuando el Usuario termine de escribir su nombre. Leálo
+			var usuario = ReadLine();
+
+			// Despliegue en la pantalla el saludo: “Hola[Nombre del Usuario]!”
+			WriteLine($"Hola {usuario}!");
+
+			// Parada Final
+			WriteLine("Bienvenid@ a C#!");
+			ReadKey();
+		}
+	}
+}
+
